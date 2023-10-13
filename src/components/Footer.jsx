11f118826facc8c522a1852ee66f781385394cc0
Madhur Narang <71398066+MadhurNarang@users.noreply.github.com>
2023-10-13 13:52:26 +0530

    All files added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className='container-max text-center py-4 border-t'>
+      Foody, made by{' '}
+      <a
+        href='https://www.linkedin.com/in/madhur-narang-a5307a210/'
+        target={'_blank'}
+        className='text-orange-500'
+      >
+        Madhur Narang
+      </a>
+    </div>
+  );
+};
+export default Footer;
